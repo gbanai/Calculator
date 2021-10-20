9125f8f986ee555684c11b2b359115f3f134f156
Gintare Banaityte <banaityte.gintare@gmail.com>
2021-10-21 00:03:41 +0300

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Calculator',
+    version='0.1.0',
+    packages=['Code'],
+    url='https://github.com/gbanai/Calculator',
+    license='MIT LICENSE',
+    author='Gintare Banaityte',
+    author_email='banaityte.gintare@gmail.com',
+    description='Calculator for basic mathematical operations'
+)
